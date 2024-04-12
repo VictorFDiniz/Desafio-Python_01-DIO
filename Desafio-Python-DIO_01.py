c3d71a34a3952a98fc3e8627c30c93e898f4c31c
victorfdiniz <victorfd80@gmail.com>
2024-04-12 15:39:01 -0300

    feat: add extrato e comentarios

--- Desafio-Python-DIO_01.py
+++ Desafio-Python-DIO_01.py
@@ -21,23 +21,23 @@ while True:
         if valor_deposito > 0:  # Verifica se o valor do depósito é positivo
             saldo += valor_deposito  # Adiciona o valor do depósito ao saldo
             extrato += f"Depósito: R${valor_deposito:.2f}\n"  # Adiciona a transação ao extrato
-            print("Depósito efetuado com sucesso!")  # Exibe mensagem de sucesso
+            print("Depósito efetuado com sucesso!")
         else:
-            print("Depósito não efetuado, valor inválido!")  # Exibe mensagem de erro
+            print("Depósito não efetuado, valor inválido!")
     elif opcao == "s":  # Opção para saque
         valor_saque = float(input("Insira o valor do saque: "))  # Solicita o valor do saque
         if valor_saque > 0 and valor_saque <= limite:  # Verifica se o valor do saque é válido
             if numero_saques == LIMITE_SAQUES:  # Verifica se o limite de saques diários foi atingido
-                print("Limite de saques diários atingido!")  # Exibe mensagem de erro
+                print("Limite de saques diários atingido!")
             elif valor_saque > saldo or saldo <= 0:  # Verifica se há saldo suficiente para o saque
-                print("Saque não efetuado, saldo insuficiente.")  # Exibe mensagem de erro
+                print("Saque não efetuado, saldo insuficiente.")
             else:
                 saldo -= valor_saque  # Subtrai o valor do saque do saldo
                 extrato += f"Saque: R${valor_saque}\n"  # Adiciona a transação ao extrato
                 numero_saques += 1  # Incrementa o número de saques realizados
-                print("Saque efetuado com sucesso!")  # Exibe mensagem de sucesso
+                print("Saque efetuado com sucesso!")
         else:
-            print("Valor de saque inválido!")  # Exibe mensagem de erro
+            print("Valor de saque inválido!")
     elif opcao == "e":  # Opção para extrato
         print("\n############### EXTRATO ###############\n")
         print("Não foram realizadas movimentações." if not extrato else extrato)  # Exibe o extrato ou mensagem de ausência de movimentações
@@ -46,4 +46,4 @@ while True:
     elif opcao == "q":  # Opção para sair do programa
         break  # Encerra o loop
     else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")  # Exibe mensagem de erro para opção inválida
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
