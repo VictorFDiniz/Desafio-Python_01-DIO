f279b51e0c40fc7fce9b78184802963a50494105
victorfdiniz <victorfd80@gmail.com>
2024-04-12 14:15:32 -0300

    add deposito e saque

--- Desafio-Python-DIO_01.py
+++ Desafio-Python-DIO_01.py
@@ -14,16 +14,31 @@ numero_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
-
     opcao = input(menu)
 
     if opcao == "d":
-        print("Depósito")
-    
-    elif opcao == "s":
-        print("Saque")
+        valor_deposito = float(input("Insira o valor do depósito: "))
+        if valor_deposito > 0:
+            saldo += valor_deposito
+            print("Depósito efetuado com sucesso!")
+        else:
+            print("Depósito não efetuado, valor inválido!")
 
-    elif opcao =="e":
+    elif opcao == "s":
+        valor_saque = float(input("Insira o valor do saque: "))
+        if valor_saque > 0 and valor_saque <= limite:
+            if numero_saques == LIMITE_SAQUES:
+                print("Limite de saques diários atingido!")
+            elif valor_saque > saldo or saldo <= 0:
+                print("Saque não efetuado, saldo insuficiente.")
+            else:
+                saldo -= valor_saque
+                numero_saques += 1
+                print("Saque efetuado com sucesso!")
+        else:
+            print("Valor de saque inválido!")
+
+    elif opcao == "e":
         print("Extrato")
 
     elif opcao == "q":
